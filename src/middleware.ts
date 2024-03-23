246cf9a6eec8854caeef3cd0d75e30121dd04ea7
Md Niyaz Alam <alamniyaz173@gmail.com>
2024-03-23 20:47:32 +0530

    middleware file added for protecting private route

---
+++ src/middleware.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  //define our public path url   
+  const isPublicPath = path==="/userauth/login" || path==="/userauth/signup";
+ //getting token from cookies using request   
+  const token = request.cookies.get('token')?.value || "";
+
+ //we will check if token is  present and user try to access login and signup then redirect to profile
+ if (isPublicPath && token){
+    return NextResponse.redirect(new URL('/profile', request.url))
+ }   
+
+ //we will check if user is not login and he also try go to profile page then redirect him in login page
+ if (!isPublicPath && !token){
+    return NextResponse.redirect(new URL("/userauth/login", request.url))
+ }  
+
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    "/profile",
+    "/userauth/login",
+    "/userauth/signup"
+  ]
+}
