092d5e704be06824b98a617cb853931793da22b5
Md Niyaz Alam <alamniyaz173@gmail.com>
2024-03-23 12:26:56 +0530

    user signup functionality is completed

--- src/app/userauth/signup/page.tsx
+++ src/app/userauth/signup/page.tsx
@@ -1,13 +1,50 @@
 "use client";
 import Link from 'next/link'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import toast, { Toaster } from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
 
 const page = () => {
-    const [userData,setUserData] = useState({
-        userName:"",
-        email:"",
-        password:""
+    const [userData, setUserData] = useState({
+        userName: "",
+        email: "",
+        password: ""
     });
+    const [btnDisabled, setBtnDisabled] = useState(true);
+    const [loading, setLoading] = useState(false);
+    const [error,setError]= useState<string  | any>("");
+    const route = useRouter();
+
+    // click function
+    async function handleSignup() {
+        try {
+            setLoading(true);
+            const resp = await axios.post("/api/users/signup",userData);
+            if(resp.statusText){
+                toast.success('user created successfully');
+                route.push("/userauth/login");
+                userData.userName="";
+                userData.email="";
+                userData.password=""
+            }
+        } catch (error:any) {
+            setError(error.response?.data?.error || "An error occurred");
+        }finally{
+            setLoading(false);
+        }
+    }
+
+    // btn will not click until user fill all data
+    useEffect(() => {
+        if (userData.userName.length > 1 && userData.email.length > 1 && userData.password.length > 1) {
+            setBtnDisabled(false)
+        }
+        else {
+            setBtnDisabled(true)
+        }
+    }, [userData])
+
     return (
         <>
             <div className='_name1'>
@@ -20,7 +57,8 @@ const page = () => {
                         id="_one1"
                         name="userName"
                         value={userData.userName}
-                        onChange={(e:any) => setUserData((prev) => ({ ...prev, userName: e.target.value }))}
+                        onChange={(e: any) => setUserData((prev) => ({ ...prev, userName: e.target.value }))}
+                        required
                     />
                 </div>
                 <div className='_name2'>
@@ -31,7 +69,8 @@ const page = () => {
                         id="_one2"
                         name="email"
                         value={userData.email}
-                        onChange={(e:any) => setUserData((prev) => ({ ...prev, email: e.target.value }))}
+                        onChange={(e: any) => setUserData((prev) => ({ ...prev, email: e.target.value }))}
+                        required
                     />
                 </div>
                 <div className='_name2'>
@@ -42,12 +81,17 @@ const page = () => {
                         id="_one3"
                         name="password"
                         value={userData.password}
-                        onChange={(e:any) => setUserData((prev) => ({ ...prev, password: e.target.value }))}
+                        onChange={(e: any) => setUserData((prev) => ({ ...prev, password: e.target.value }))}
+                        required
                     />
                 </div>
-                <button className='_name3'>SignUp</button>
+                {error && <span style={{display:"block",color:"red",textAlign:"center",fontSize:"12px",margin:"0px 4px 6px"}}>{error}</span>}
+                <button className='_name3' onClick={handleSignup}
+                    disabled={btnDisabled ? true : false}
+                    style={{ opacity:`${btnDisabled?".6":"1"}`,cursor:`${btnDisabled ? "no-drop" : "pointer"}`}}>{loading?"Processing...":"SignUp"}</button>
                 <Link href="/userauth/login" className='_name4'>Already have account? <span>Login</span></Link>
             </div>
+            <Toaster />
         </>
     )
 }
