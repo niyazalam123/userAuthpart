3e9da041c580bd8c4776f7bc647c4a716cc643c9
Md Niyaz Alam <alamniyaz173@gmail.com>
2024-03-25 05:21:29 +0530

    forgot password api is completed

---
+++ src/app/api/users/forgotpassword/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse,NextRequest } from "next/server";
+import User from "@/models/signupModel";
+import { mongoDbConnect } from "@/dbConfig/dbConfig";
+import { sendMail } from "@/helpers/mailers";
+
+export async function POST(request:NextRequest){
+    try {
+        const reqBody = await request.json();
+        const {email} = reqBody;
+
+        // validation , user find based on incoming email
+        const user = await User.findOne({email});
+        if (!user){
+            return NextResponse.json({error:"User not found"},{status:401});
+        }
+        // Send email to user with the password reset link
+        await sendMail({
+            email: email,
+            emailType: "FORGOT",
+            userId: user._id,
+        });
+        return NextResponse.json({message:"Forgot password token created successfully",success:true},{status:200})
+
+    } catch (error) {
+        return NextResponse.json({error:error},{status:500})
+    }
+}
--- src/app/userauth/forgotpassword/page.tsx
+++ src/app/userauth/forgotpassword/page.tsx
@@ -1,7 +1,17 @@
 "use client";
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
+import toast, { Toaster } from 'react-hot-toast';
 
 const page = () => {
+    const [email,setEmail] = useState("");
+    const [loading,setLoading] = useState(false);
+    const [error,seterror] = useState("");
+
+    async function handleForgotPassword(){
+        
+    }
+
     return (
         <>
             <div className='_frgtp1'>
@@ -11,12 +21,13 @@ const page = () => {
                 </div>
                 <div className='_frgtp3'>
                     <div className='_frgtp4'>
-                        <label htmlFor="_one1">Enter UserName</label>
-                        <input type="text" placeholder='Enter UserName...' id="_one1" />
+                        <label htmlFor="_one1">Enter Email</label>
+                        <input type="email" placeholder='Enter Email...' id="_one1"  onChange={(e)=>setEmail(e.target.value)} name="email" value={email} />
                     </div>
-                    <button className='_frgtp5'>Forgot password</button>
+                    <button className='_frgtp5' onClick={handleForgotPassword}>{loading?"Processing...":"Forgot password"}</button>
                 </div>
             </div>
+            <Toaster />
         </>
     )
 }
